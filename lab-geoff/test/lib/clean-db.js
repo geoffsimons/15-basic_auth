fa21bc4fe5a06e0463093aaa7c2f772dd7e4fccf
Geoff Simons <geoff.simons@gmail.com>
2017-01-05 11:39:11 -0800

    Wrapping in Promise.all

--- lab-geoff/test/lib/clean-db.js
+++ lab-geoff/test/lib/clean-db.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const debug = require('debug')('mnp:clean-db');
+// const debug = function(msg) { console.log(msg); };
 const User = require('../../model/user.js');
 const Game = require('../../model/game.js');
 const Pic = require('../../model/pic.js');
+const Promise = require('bluebird');
+
+//NOTE: We are not connecting to mongoose here
+//      because it's assumed that the server will
+//      be running.
 
 module.exports = function() {
-  User.remove({});
-  Game.remove({});
-  Pic.remove({});
+  debug('working...');
+
+  return Promise.all([
+    User.remove({}),
+    Game.remove({}),
+    Pic.remove({})
+  ]);
 };
