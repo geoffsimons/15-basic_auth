cde2f8ac6bc312926aa9afea1a03030d2df3757f
Geoff Simons <geoff.simons@gmail.com>
2017-01-04 23:29:10 -0800

    POST and DELETE tests, DEL does not pass

--- lab-geoff/test/pic-route-test.js
+++ lab-geoff/test/pic-route-test.js
@@ -4,35 +4,75 @@ const expect = require('chai').expect;
 const request = require('superagent');
 const debug = require('debug')('mnp:pic-route-test');
 
-const Pic = require('../model/pic.js');
-const User = require('../model/user.js');
 const Game = require('../model/game.js');
 
 const serverToggle = require('./lib/server-toggle.js');
 const server = require('../server.js');
+
+const mockUser = require('./lib/mock-user.js');
 
 const url = `http://localhost:${process.env.PORT}`;
 
 describe('Pic Routes', function() {
   before( done => serverToggle.start(server, done));
   after( done => serverToggle.stop(server,done));
+  after(require('./lib/clean-db.js'));
 
-  //TODO: Add cleanup
+  describe('POST /api/game/:gameId/pic', () => {
+    describe('with a valid token and data', () => {
+      before( done => {
+        mockUser()
+        .then( user => {
+          this.tempUser = user;
+          done();
+        })
+        .catch(done);
+      });
+      before( done => {
+        new Game({
+          userId: this.tempUser._id,
+          type: 'singles',
+          players: [ this.tempUser._id ]
+        }).save()
+        .then( game => {
+          this.tempGame = game;
+          done();
+        })
+        .catch(done);
+      });
 
-  describe('POST /api/game/:gameId/pic', function() {
-    describe('with a valid token and data', function() {
-      //TODO: make example users
-      //TODO: make an example game
       it('should return a pic object', done => {
-        done(false);
+        request.post(`${url}/api/game/${this.tempGame._id}/pic`)
+        .set({
+          Authorization: `Bearer ${this.tempUser.token}`
+        })
+        .attach('image', `${__dirname}/data/tester.jpeg`)
+        .end( (err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.userId).to.equal(this.tempUser._id.toString());
+          expect(res.body.gameId).to.equal(this.tempGame._id.toString());
+          expect(res.body.imageURI).to.be.a('string'); //to be a 'url' ??
+          expect(res.body.objectKey).to.be.a('string');
+          this.tempPic = res.body;
+          debug('created ', this.tempPic._id);
+          done();
+        });
       });
     }); // valid token and data
   }); // POST /api/game/:gameId/pic
 
-  describe('DELETE /api/game/:gameId/pic/:picId', function() {
-    describe('with valid ids', function() {
+  describe('DELETE /api/game/:gameId/pic/:picId', () => {
+    describe('with valid ids', () => {
       it('should return a 204', done => {
-        done(false);
+        debug('trying to delete',this.tempPic._id);
+        request.delete(`${url}/api/game/${this.tempGame._id}/pic/${this.tempPic._id}`)
+        .set({
+          Authorization: `Bearer ${this.tempUser.token}`
+        })
+        .end( (err, res) => {
+          expect(res.status).to.equal(204);
+          done();
+        });
       });
     }); // valid ids
   }); // DELETE /api/game/:gameId/pic/:picId
