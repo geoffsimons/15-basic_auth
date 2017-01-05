8ebae3b85824a1bbedab2706a01611bd14360d78
Geoff Simons <geoff.simons@gmail.com>
2017-01-05 01:00:39 -0800

    Tried showing more error details

--- lab-geoff/lib/error-middleware.js
+++ lab-geoff/lib/error-middleware.js
@@ -4,6 +4,8 @@ const createError = require('http-errors');
 const debug = require('debug')('mnp:error-middleware');
 
 module.exports = function(err, req, res, next) {
+  // console.error(err);
+
   if(err.status) {
     debug('user error:',err.name);
     res.status(err.status).send(err.name);
